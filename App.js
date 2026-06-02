86d4507114fe4520965479029e28550047400399
Nisha <raynisha177@gmail.com>
2026-06-02 08:23:34 -0400

    Add Nisha intro to App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+// Team Introductions
+
+// Nisha
+// Based in Dallas, living with her husband and dog.
+// When not working or learning to code, she enjoys traveling, trying new restaurants,
+// and recently picked up a new hobby: puzzles!
+
+// Teammate 2 - Add your intro here
+// Teammate 3 - Add your intro here
