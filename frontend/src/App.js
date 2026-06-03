7c1dbc2123d1520266881f9100677ee4effea8b4
Sonam <pjsonam@gmail.com>
2026-06-03 12:37:10 +0530

    create initial JobEase repository structure

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        JobEase
+        <hr />
+      </header>
+    </div>
+  );
+}
+
+export default App;
