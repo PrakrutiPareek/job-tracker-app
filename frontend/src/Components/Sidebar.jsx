b2352734136798b9ffe60cb00f7ee54c47555f95
prakruti pareek <prakruti.kapadia@gmail.com>
2026-06-14 01:17:15 +0100

    Initial static application page created

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
 
   return (
     <aside
-      className="w-[256px] border-r h-full"
+      className="w-[256px] border-r h-screen"
       aria-label="Secondary navigation"
     >
       <nav
