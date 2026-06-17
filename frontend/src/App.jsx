7b592861a85837ea1e91bec4ae55549e11096568
Nisha <raynisha177@gmail.com>
2026-06-17 14:50:55 -0500

    Add Firebase authentication for login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,33 @@
-import {Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//pages
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
+// existing pages
+import Applications from "./Pages/Applications";
 import Profile from "./Pages/Profile";
-import SavedJobs from "./Pages/Applications";
+import JobSearch from "./Pages/JobSearch";
+import PageNotFound from "./Pages/PageNotFound";
+
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
-import JobSearch from "./Pages/JobSearch";
-import {ToastContainer} from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-//layouts
-import MainLayout from "./Components/MainLayout";
-import UserLayout from "./Components/UserLayout";
-import PageNotFound from "./Pages/PageNotFound";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
-    <>
-      <ToastContainer position="top-right" />{" "}
-      {/* Add the ToastContainer component to enable toast notifications */}
+    <Router>
       <Routes>
-        {/* MainLayout for Navbar */}
-        <Route path="/" element={<MainLayout />}>
-          {/* Auth pages WITH navbar/footer */}
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="jobsearch" element={<JobSearch />} />
-          <Route path="savedjobs" element={<SavedJobs />} />
-        </Route>
+        {/* Main routes */}
+        <Route path="/" element={<JobSearch />} />
+        <Route path="/applications" element={<Applications />} />
+        <Route path="/profile" element={<Profile />} />
 
-        {/* UserLayout for Sidebar */}
-        <Route path="/profile" element={<UserLayout />}>
-          <Route index element={<Profile />} />
-          <Route path="jobsearch" element={<JobSearch />} />
-          <Route path="savedjobs" element={<SavedJobs />} />
-        </Route>
-        {/* UserLayout for Sidebar */}
-        <Route path="/profile" element={<UserLayout />}>
-          <Route index element={<Profile />} />
-          <Route path="jobsearch" element={<JobSearch />} />
-          <Route path="savedjobs" element={<SavedJobs />} />
-        </Route>
-
-        {/* Auth pages WITHOUT navbar/footer */}
+        {/* Auth routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Fallback */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
