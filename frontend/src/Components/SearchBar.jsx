382c06bf5cb9d008a993e5324c67de7d70841095
Sonam <pjsonam@gmail.com>
2026-06-21 18:56:49 +0530

    modified placeholdertext in SearchBar component

--- frontend/src/Components/SearchBar.jsx
+++ frontend/src/Components/SearchBar.jsx
@@ -1,29 +1,48 @@
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar =
-  ({ keyword, location, setKeyword, setLocation, onSearch }) => {
-    console.log("keyword", keyword);
-    return (
-      <div className="bg-(--navy-blue) rounded-2xl flex items-center p-4">
-        <input
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-          placeholder="Seach by Role"
-          className="bg-transparent flex-1 outline-none"
-        />
+const SearchBar = ({
+  keyword,
+  location,
+  setKeyword,
+  setLocation,
+  onSearch,
+}) => {
+  console.log("keyword", keyword);
+  return (
+    <div className="bg-(--navy-blue) rounded-2xl flex items-center p-4">
+      <input
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onSearch();
+          }
+        }}
+        placeholder="Search by Role"
+        className="bg-transparent flex-1 outline-none"
+      />
 
-        <div className="w-px h-8 bg-(--white) mx-4"></div>
-        <input
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          placeholder="Search by Location"
-          className="bg-transparent flex-1 outline-none"
-        />
-        <button onClick={onSearch}>
-          <FaSearch size={24} />
-        </button>
-      </div>
-    );
-  };
+      <div className="w-px h-8 bg-(--white) mx-4"></div>
+      <input
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onSearch();
+          }
+        }}
+        placeholder="Search jobs in UK only"
+        className="bg-transparent flex-1 outline-none"
+      />
+      <button
+        onClick={onSearch}
+        className="ml-4 cursor-pointer"
+        aria-label="Search Jobs "
+      >
+        <FaSearch size={24} />
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
