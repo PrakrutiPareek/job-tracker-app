910a7786db2708ced8ac89927fbc4a8735adff27
Sonam <pjsonam@gmail.com>
2026-06-23 13:02:33 +0530

    refactor: removed bg and border styling in ErrorMessage and improved styling of Apply & Save buttons in JobCard component

--- frontend/src/Components/ErrorMessage.jsx
+++ frontend/src/Components/ErrorMessage.jsx
@@ -2,7 +2,7 @@ const ErrorMessage = ({ message }) => {
   if (!message) return null;
 
   return (
-    <div className="font-bold  text-xl text-red-400 rounded-lg p-8 mb-8 text-center bg-(--navy-blue)">
+    <div className="font-bold  text-xl text-red-400 rounded-lg p-4 mb-4 text-center">
       {message}
     </div>
   );
--- frontend/src/Components/JobCard.jsx
+++ frontend/src/Components/JobCard.jsx
@@ -2,38 +2,36 @@ const JobCard = ({ job }) => {
   const { company, title, location, redirect_url } = job;
 
   return (
-    <div className="rounded-xl border border-slate-700 p-5 shadow-sm
-      transition-transform hover:-translate-y-1">
-      <span className="rounded-full bg-(--navy-blue) px-3 py-1 text-sm
-        font-medium text-(--navy-blue)">
+    <div
+      className="rounded-xl border border-slate-700 p-5 shadow-sm
+      transition-transform hover:-translate-y-1"
+    >
+      <span
+        className="rounded-full bg-(--navy-blue) px-3 py-1 text-sm
+        font-medium text-(--navy-blue)"
+      >
         {company.display_name}
       </span>
 
-      <h3 className="mt-3 text-xl font-bold text-(--white)">
-        {title}
-      </h3>
+      <h3 className="mt-3 text-xl font-bold text-(--white)">{title}</h3>
 
-      <p className="mt-2 text-slate-400">
-        📍 {location.display_name}
-      </p>
+      <p className="mt-2 text-slate-400">📍 {location.display_name}</p>
 
       <div className="mt-6 flex items-center justify-between">
-        <span className="text-sm text-(--gray)">
-          Remote Friendly
-        </span>
+        <span className="text-sm text-(--gray)">Remote Friendly</span>
 
         <div className="flex gap-3">
           <button
-            className="rounded-lg border text-(--black) px-4 py-2
-              text-sm  bg-(--white) font-medium hover:bg-(--gray) hover:text-(--black) transition"
+            className="rounded-lg bg-(--yellow) px-4 py-2 text-sm
+                font-semibold text-(--black)"
           >
             Save
           </button>
 
           <a href={redirect_url} target="_blank" rel="noreferrer">
             <button
-              className="rounded-lg bg-(--yellow) px-4 py-2 text-sm
-                font-semibold text-(--black)"
+              className="rounded-lg border text-(--black) px-4 py-2
+              text-sm  bg-(--white) font-medium hover:bg-(--gray) hover:text-(--black) transition"
             >
               Apply
             </button>
