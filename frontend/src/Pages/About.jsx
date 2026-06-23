72bd203c8b50d408c82b8a4aad1af8a3ea21d973
codesbymonique <moniquekhahin163@hotmail.com>
2026-06-23 23:44:46 +0100

    Made changes based on feedback

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,31 +1,33 @@
-// Import the icon from lucide
-import { UserRound } from 'lucide-react';
-
 // Team members information: id, name and bio
 const teamMembers = [
   {
     id: 1, 
-    name: 'Monique', 
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget semper elit.',
+    name: 'Monique',
+    initials: 'MK', 
+    bio: 'Hi, I am Monique and I started learning to code during maternity leave this motivated me to apply to CFG',
   },
   {
     id: 2,
     name: 'Nisha',
+    initials: 'NR',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget semper elit.',
   },
   {
     id: 3,
     name: 'Prakruti',
+    initials: 'PP',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget semper elit.',
   },
   {
     id: 4,
     name: 'Sonam',
+    initials: 'SJ',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget semper elit.',
   },
   {
     id: 5,
     name: 'Tara',
+    initials: 'TO',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget semper elit.',
   },
 ];
@@ -40,13 +42,12 @@ const About = () => {
       <div className="text-center mb-12">
 
         {/* Page title */}
-        <h1 className="text-(--yellow) uppercase font-bold tracking-widest text-3xl mb-4"> Meet the team</h1>
+        <h1 className="font-headings text-(--yellow) uppercase font-bold tracking-widest text-3xl mb-4"> Meet the team</h1>
 
         {/* Page paragraph with description of the app */}
-        <p className="text-(--gray) max-w-2xl mx-auto text-base">
-          Code First Girls(CFG) Degree alumni who are passionate about development. We created
-          this simple react web app to help others stay organised to track
-          their job application progress.</p>
+          <p className="text-(--gray) max-w-2xl mx-auto text-base">
+          Code First Girls(CFG) Degree alumni who are passionate about development. We created this simple react web app to help others stay organised to track their job application progress.
+          </p>
       </div>
       
       {/* Team members cards loop using teamMembers array and creates a card for each member */}
@@ -58,7 +59,9 @@ const About = () => {
 >
             {/* Photo placeholder for each member using icon from lucide-react */}
             <div className="w-full h-36 bg-gray-500 flex items-center justify-center">
-              <UserRound size={48} className="text-gray-300" />
+              <div className= "text-white text-4xl font-bold font-headings">
+                {member.initials}
+                </div>
             </div>
 
             {/* Card information */}
@@ -73,14 +76,6 @@ const About = () => {
           </div>
         ))}
       </div>
-
-      {/* Bottom tagline */}
-      <div className="bg-(--black) text-center py-6 border-t border-gray-800">
-        <p className="text-(--yellow) font-bold text-xl">
-          Start feeling at ease with JobEase
-        </p>
-      </div>
-
     </main>
   );
 };
