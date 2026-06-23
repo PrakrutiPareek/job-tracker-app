c2ad6dee22814b45fbb8d7f82ee58449929a58a2
codesbymonique <moniquekhahin163@hotmail.com>
2026-06-24 00:42:20 +0100

    made recommended changes

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
       <div className="bg-(--navy-blue) rounded-xl px-12 py-10 text-center max-w-lg w-full mx-4">
 
         {/* Main heading */}
-        <h1 className="text-(--yellow) font-bold text-3xl mb-3">Get In Touch</h1>
+        <h1 className="font-headings text-(--yellow) font-bold text-3xl mb-3">Get In Touch</h1>
 
         {/* Paragraph for subtitle text */}
         <p className="text-(--gray) text-sm mb-4">If you have any questions and feedback feel free to contact us</p>
@@ -25,7 +25,7 @@ const Contact = () => {
           <span className="text-(--white) font-semibold">Email Us</span>
 
           {/* Added clickable email address that will open default email app, change colour on hover*/}
-            <a href="mailto:JobEase@email.com"className="text-(--gray) hover:text-(--yellow) underline transition-            colors">JobEase@email.com</a>
+            <a href="mailto:JobEase@email.com"className="text-(--gray) hover:text-(--yellow) underline transition-colors">JobEase@email.com</a>
 
         </div>
       </div>
